80d1e83b93573c92c12a6425d80fd3bd4fa90755
shubhra tiwary <shubhratiwary5@gmail.com>
2025-01-03 00:53:50 +0530

    changed the url

--- src/keycloak.ts
+++ src/keycloak.ts
@@ -1,12 +1,9 @@
-
-import Keycloak from 'keycloak-js';
-
+import Keycloak from "keycloak-js";
 
 const keycloak = new Keycloak({
-  url:'http://localhost:8080',
-  realm:'WhiteBoard',
-  clientId:'WhiteBoard',
+  url: "http://localhost:8081",
+  realm: "WhiteBoard",
+  clientId: "WhiteBoard",
 });
-
 
 export default keycloak;
