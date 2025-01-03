587a3669987d90eec1f630e9fc56cd05e73a1e14
shubhra tiwary <shubhratiwary5@gmail.com>
2025-01-04 03:11:20 +0530

    Keycloak removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,17 @@
 import React, { Suspense, useEffect, useRef } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import useAuthStore from "./storage/authStore";
 import LoadingScreen from "./components/LoadingScreen";
 
 const InitialPage = React.lazy(() => import("./components/InitialPage"));
 const Room = React.lazy(() => import("./components/Room"));
-const ProtectedRoutes = React.lazy(
-  () => import("./components/ProtectedRoutes")
-);
 
 const App: React.FC = () => {
-  const isRun = useRef(false);
-  const { initKeycloak, error } = useAuthStore();
-
-  useEffect(() => {
-    if (isRun.current) return;
-    isRun.current = true;
-    const initializeKeycloak = async () => {
-      await initKeycloak();
-    };
-    initializeKeycloak();
-  }, [initKeycloak]);
-
-  if (error) {
-    return <div>Error initializing authentication: {error}</div>;
-  }
-
   return (
     <BrowserRouter>
       <Suspense fallback={<LoadingScreen />}>
         <Routes>
-          <Route
-            path="/"
-            element={<ProtectedRoutes element={<InitialPage />} />}
-          />
-          <Route
-            path="/room/:roomID"
-            element={<ProtectedRoutes element={<Room />} />}
-          />
+          <Route path="/" element={<InitialPage />} />
+          <Route path="/room/:roomID" element={<Room />} />
         </Routes>
       </Suspense>
     </BrowserRouter>
